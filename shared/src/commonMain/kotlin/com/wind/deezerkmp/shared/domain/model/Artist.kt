ec75470acc4c2f020f226a2cf468ea71da0874f5
Phong Huỳnh <p7157568@gmail.com>
2020-11-15 16:29:20 +0700

    Artist list by genre - create api and model

--- shared/src/commonMain/kotlin/com/wind/deezerkmp/shared/domain/model/Artist.kt
+++ shared/src/commonMain/kotlin/com/wind/deezerkmp/shared/domain/model/Artist.kt
@@ -5,13 +5,10 @@ package com.wind.deezerkmp.shared.domain.model
  * Created by Phong Huynh on 11/5/2020
  */
 data class Artist(
-    private val person: PersonModel,
-    private val picture: String,
-    private val pictureBig: String,
-    private val pictureMedium: String,
-    private val pictureSmall: String,
-    private val pictureXl: String,
+    private val id: String,
+    private val model: DeezerBaseModel,
     private val radio: Boolean,
-) : Person {
-    override fun getPerson() = person
+    private val trackList: String
+) {
+    fun isValid() = model.isValid() && id.isNotEmpty()
 }
--- shared/src/commonMain/kotlin/com/wind/deezerkmp/shared/domain/model/User.kt
+++ shared/src/commonMain/kotlin/com/wind/deezerkmp/shared/domain/model/User.kt
@@ -3,6 +3,9 @@ package com.wind.deezerkmp.shared.domain.model
 /**
  * Created by Phong Huynh on 11/5/2020
  */
-data class User(private val person: PersonModel): Person {
-    override fun getPerson() = person
-}
+data class User(
+    private val id: String,
+    private val name: String,
+    private val trackList: String,
+    private val type: String
+)
